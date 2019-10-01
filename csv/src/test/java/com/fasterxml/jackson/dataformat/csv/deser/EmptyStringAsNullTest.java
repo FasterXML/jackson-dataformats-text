373e8b1b396b3befeaa58ae19b95365a0c1a0fff
Tyler Carpenter-Rivers <tcarpent@meredith.com>
2019-09-30 17:30:53 -0700

    (csv) Support coercion of empty strings to null objects
    https://github.com/FasterXML/jackson-dataformats-text/issues/7

---
+++ csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/EmptyStringAsNullTest.java
@@ -0,0 +1,65 @@
+package com.fasterxml.jackson.dataformat.csv.deser;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link CsvParser.Feature#EMPTY_STRING_AS_NULL}
+ */
+public class EmptyStringAsNullTest {
+
+
+    @JsonPropertyOrder({"firstName", "middleName", "lastName"})
+    static class TestUser {
+        public String firstName, middleName, lastName;
+    }
+
+    @Test
+    public void givenFeatureDisabledByDefault_whenColumnIsEmptyString_thenParseAsEmptyString() throws IOException {
+        // setup test data
+        TestUser expectedTestUser = new TestUser();
+        expectedTestUser.firstName = "Grace";
+        expectedTestUser.middleName = "";
+        expectedTestUser.lastName = "Hopper";
+        CsvMapper csvMapper = CsvMapper.builder().build();
+        ObjectReader objectReader = csvMapper.readerFor(TestUser.class).with(csvMapper.schemaFor(TestUser.class));
+        String csv = "Grace,,Hopper";
+
+        // execute
+        TestUser actualTestUser = objectReader.readValue(csv);
+
+        // test
+        assertNotNull(actualTestUser);
+        assertEquals(expectedTestUser.firstName, actualTestUser.firstName);
+        assertEquals(expectedTestUser.middleName, actualTestUser.middleName);
+        assertEquals(expectedTestUser.lastName, actualTestUser.lastName);
+    }
+
+    @Test
+    public void givenFeatureEnabled_whenColumnIsEmptyString_thenParseAsNull() throws IOException {
+        // setup test data
+        TestUser expectedTestUser = new TestUser();
+        expectedTestUser.firstName = "Grace";
+        expectedTestUser.lastName = "Hopper";
+        CsvMapper csvMapper = CsvMapper.builder().enable(CsvParser.Feature.EMPTY_STRING_AS_NULL).build();
+        ObjectReader objectReader = csvMapper.readerFor(TestUser.class).with(csvMapper.schemaFor(TestUser.class));
+        String csv = "Grace,,Hopper";
+
+        // execute
+        TestUser actualTestUser = objectReader.readValue(csv);
+
+        // test
+        assertNotNull(actualTestUser);
+        assertEquals(expectedTestUser.firstName, actualTestUser.firstName);
+        assertNull("The column that contains an empty String should be deserialized as null ", actualTestUser.middleName);
+        assertEquals(expectedTestUser.lastName, actualTestUser.lastName);
+    }
+}
