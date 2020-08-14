1779911feba517b80ff144aa29d33d519973b1c6
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-08-13 18:18:22 -0700

    Add failing test for #215

---
+++ yaml/src/test/java/com/fasterxml/jackson/dataformat/yaml/failing/SimpleGeneration215Tests.java
@@ -0,0 +1,21 @@
+package com.fasterxml.jackson.dataformat.yaml.failing;
+
+import java.util.Collections;
+
+import com.fasterxml.jackson.dataformat.yaml.ModuleTestBase;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+public class SimpleGeneration215Tests extends ModuleTestBase
+{
+    // [dataformats-text#215]: setting used in constructor
+    public void testStartMarkerViaWriter() throws Exception
+    {
+        final String output = YAMLMapper.builder().build()
+            .writer()
+            .without(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+            .writeValueAsString(Collections.singletonMap("key", "value"))
+            .trim();
+        assertEquals("key: \"value\"", output);
+    }
+}
