53cf2636d16c120afef1043887fefba1310ff0c2
Matteo Giordano <Matteo.Giordano@eng.it>
2022-11-17 04:49:47 +0100

    Allow missing header columns with `Feature.FAIL_ON_MISSING_HEADER_COLUMNS` (#353)

--- csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/MissingColumns285Test.java
+++ csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/MissingColumns285Test.java
@@ -20,14 +20,17 @@ public class MissingColumns285Test extends ModuleTestBase
      */
 
     private final CsvMapper MAPPER = mapperForCsv();
+    private final CsvSchema csvSchema = CsvSchema.builder()
+            .setUseHeader(true)
+            .setReorderColumns(true)
+            .addColumn("name")
+            .addColumn("age")
+            .build();
+    private final String CSV = "name\nRoger\n";
 
-    // [dataformats-text#285]
-    public void testMissingWithReorder() throws Exception
+    // [dataformats-text#285]: fail by default
+    public void testFailOnMissingWithReorder() throws Exception
     {
-        CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).setReorderColumns(true)
-                .addColumn("name").addColumn("age").build();
-        final String CSV = "name\n"
-                +"Roger\n";
         // Need to have it all inside try block since construction tries to read
         // the first token
         try {
@@ -40,5 +43,20 @@ public class MissingColumns285Test extends ModuleTestBase
         } catch (CsvReadException e) {
             verifyException(e, "Missing 1 header column: [\"age\"]");
         }
+    }
+
+    // [dataformats-text#285]: optionally allow
+    public void testAllowMissingWithReorder() throws Exception
+    {
+        MappingIterator<Map<String, Object>> it = MAPPER
+                .readerFor(Map.class)
+                .with(csvSchema)
+                .without(CsvParser.Feature.FAIL_ON_MISSING_HEADER_COLUMNS)
+                .readValues(CSV);
+        assertTrue(it.hasNext());
+        Map<?, ?> result = it.nextValue();
+        assertEquals(1, result.size());
+        assertEquals("Roger", result.get("name"));
+        assertNull(result.get("age"));
     }
 }
