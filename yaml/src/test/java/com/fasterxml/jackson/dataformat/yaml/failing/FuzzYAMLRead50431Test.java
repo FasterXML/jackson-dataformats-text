28e3b69d5e44d85f7ce3119ea64b66ac94c4a3d9
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-03-07 17:55:35 -0800

    Add failing test for #400 (yaml, char decoding) (#401)

---
+++ yaml/src/test/java/com/fasterxml/jackson/dataformat/yaml/failing/FuzzYAMLRead50431Test.java
@@ -0,0 +1,29 @@
+package com.fasterxml.jackson.dataformat.yaml.failing;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.ModuleTestBase;
+
+/**
+ * Reproduction of:
+ *
+ * https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=50431
+ */
+public class FuzzYAMLRead50431Test extends ModuleTestBase
+{
+    private final ObjectMapper YAML_MAPPER = newObjectMapper();
+
+    // https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=50431
+    public void testUnicodeDecoding50431() throws Exception
+    {
+        String input = "\n\"\\UE30EEE";
+        try {
+            YAML_MAPPER.readTree(input);
+            fail("Should not pass");
+        } catch (StreamReadException e) {
+            // Not sure what to verify, but should be exposed as one of Jackson's
+            // exceptions (or possibly IOException)
+            verifyException(e, "Not a valid Unicode code point: 0xE30EEE");
+        }
+    }
+}
