b46fadaef7508b8d7501e48ff4cbebabb1abb22d
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-08 10:54:54 +0100

    yaml test

---
+++ yaml/src/test/java/com/fasterxml/jackson/dataformat/yaml/ser/dos/CyclicDataSerTest.java
@@ -0,0 +1,30 @@
+package com.fasterxml.jackson.dataformat.yaml.ser.dos;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.ModuleTestBase;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple unit tests to verify that we fail gracefully if you attempt to serialize
+ * data that is cyclic (eg a list that contains itself).
+ */
+public class CyclicDataSerTest extends ModuleTestBase
+{
+    private final ObjectMapper MAPPER = YAMLMapper.builder().build();
+
+    public void testListWithSelfReference() throws Exception {
+        List<Object> list = new ArrayList<>();
+        list.add(list);
+        try {
+            MAPPER.writeValueAsString(list);
+            fail("expected JsonMappingException");
+        } catch (JsonMappingException jmex) {
+            assertTrue("JsonMappingException message is as expected?",
+                    jmex.getMessage().startsWith("Document nesting depth (1001) exceeds the maximum allowed"));
+        }
+    }
+}
