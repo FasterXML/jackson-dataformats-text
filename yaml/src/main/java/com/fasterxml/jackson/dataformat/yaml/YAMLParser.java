f913d174b288959ab51ff6ab1d8f469a1114f5bb
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-10-12 20:41:18 -0700

    Fix issue #435 (from OSS-Fuzz) (#436)

--- yaml/src/main/java/com/fasterxml/jackson/dataformat/yaml/YAMLParser.java
+++ yaml/src/main/java/com/fasterxml/jackson/dataformat/yaml/YAMLParser.java
@@ -699,6 +699,7 @@ public class YAMLParser extends ParserBase
         if (len == i) { // should not occur but play it safe
             return null;
         }
+
         // Next: either "0" ("-0" and "+0" also accepted), or non-decimal. So:
         if (value.charAt(i) == '0') {
             if (++i == len) {
@@ -1190,7 +1191,7 @@ public class YAMLParser extends ParserBase
      * using standard JDK classes.
      * Currently this just means stripping out optional underscores.
      */
-    private JsonToken _cleanYamlInt(String str)
+    private JsonToken _cleanYamlInt(String str) throws IOException
     {
         // Here we already know there is either plus sign, or underscore (or both) so
         final int len = str.length();
@@ -1204,6 +1205,9 @@ public class YAMLParser extends ParserBase
             }
         }
         _cleanedTextValue = sb.toString();
+        if (_cleanedTextValue.isEmpty()) {
+            _reportError(String.format("Invalid number ('%s')", str));
+        }
         return JsonToken.VALUE_NUMBER_INT;
     }
 
