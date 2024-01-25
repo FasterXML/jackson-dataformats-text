2bcb8679f366e3a9a56f89586b07fd50906d48e5
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-25 14:47:14 -0800

    Add writer-side max-nesting constraints checking test

--- yaml/src/test/java/com/fasterxml/jackson/dataformat/yaml/constraints/DeeplyNestedYAMLReadWriteTest.java
+++ yaml/src/test/java/com/fasterxml/jackson/dataformat/yaml/constraints/DeeplyNestedYAMLReadWriteTest.java
@@ -2,34 +2,38 @@ package com.fasterxml.jackson.dataformat.yaml.constraints;
 
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.exc.StreamConstraintsException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.ModuleTestBase;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 /**
- * Unit test(s) for verifying handling of new (in 2.17 for YAML)
- * StreamReadConstraints wrt maximum nesting depth.
+ * Unit test(s) for verifying handling of maximum nesting depth
+ * for reading (StreamReadConstraints) and writing (StreamWriteConstraints).
  */
-public class DeeplyNestedYAMLReadTest
+public class DeeplyNestedYAMLReadWriteTest
     extends ModuleTestBase
 {
     private final YAMLMapper YAML_MAPPER = new YAMLMapper(
             YAMLFactory.builder()
+            // Use higher limit for writing to simplify testing setup
                 .streamReadConstraints(StreamReadConstraints.builder()
                     .maxNestingDepth(10).build())
+                .streamWriteConstraints(StreamWriteConstraints.builder()
+                        .maxNestingDepth(12).build())
             .build()
             );
 
-    public void testDeepNestingStreaming() throws Exception
+    public void testDeepNestingRead() throws Exception
     {
-        final String DOC = createDeepNestedDoc(10);
+        final String DOC = YAML_MAPPER.writeValueAsString(createDeepNestedDoc(11));
         try (JsonParser p = YAML_MAPPER.createParser(DOC)) {
-            _testDeepNesting(p);
+            _testDeepNestingRead(p);
         }
     }
 
-    private void _testDeepNesting(JsonParser p) throws Exception
+    private void _testDeepNestingRead(JsonParser p) throws Exception
     {
         try {
             while (p.nextToken() != null) { }
@@ -40,7 +44,20 @@ public class DeeplyNestedYAMLReadTest
         }
     }
     
-    private String createDeepNestedDoc(final int depth) throws Exception
+    public void testDeepNestingWrite() throws Exception
+    {
+        final JsonNode docRoot = createDeepNestedDoc(13);
+        try {
+            YAML_MAPPER.writeValueAsString(docRoot);
+            fail("Should not pass");
+        } catch (StreamConstraintsException e) {
+            e.printStackTrace();
+            assertEquals("Document nesting depth (13) exceeds the maximum allowed (12, from `StreamWriteConstraints.getMaxNestingDepth()`)",
+                    e.getMessage());
+        }
+    }
+
+    private JsonNode createDeepNestedDoc(final int depth) throws Exception
     {
         final ObjectNode root = YAML_MAPPER.createObjectNode();
         ObjectNode curr = root;
@@ -48,6 +65,6 @@ public class DeeplyNestedYAMLReadTest
             curr = curr.putObject("nested"+i);
         }
         curr.put("value", 42);
-        return YAML_MAPPER.writeValueAsString(root);
+        return root;
     }
 }
