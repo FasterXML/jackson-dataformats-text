7c21f8abe0b8da101e1d9dc8afc0a97b60283ff0
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-03-01 19:55:49 -0800

    Fix unit test wrt new fast-fp-read/write defaults

--- csv/src/test/java/tools/jackson/dataformat/csv/CSVFactoryFeaturesTest.java
+++ csv/src/test/java/tools/jackson/dataformat/csv/CSVFactoryFeaturesTest.java
@@ -35,12 +35,12 @@ public class CSVFactoryFeaturesTest extends ModuleTestBase
                 .enable(StreamWriteFeature.USE_FAST_DOUBLE_WRITER)
                 .build();
         assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
         assertTrue(f.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
         final CsvMapper mapper = mapperForCsv(f);
         JsonParser parser = mapper.createParser(new StringReader(""));
         assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
         JsonGenerator generator = mapper.createGenerator(new StringWriter());
         assertTrue(generator.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
     }
@@ -48,43 +48,44 @@ public class CSVFactoryFeaturesTest extends ModuleTestBase
     public void testFactoryFastBigNumberFeature() throws Exception
     {
         CsvFactory f = CsvFactory.builder()
-                .enable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
+                .disable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
                 .build();
-        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
         final CsvMapper mapper = mapperForCsv(f);
         JsonParser parser = mapper.createParser(new StringReader(""));
-        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
     }
 
     public void testFactoryBuilderFastFeatures() throws Exception
     {
         CsvFactory f = CsvFactory.builder()
-            .enable(StreamReadFeature.USE_FAST_DOUBLE_PARSER)
-            .enable(StreamWriteFeature.USE_FAST_DOUBLE_WRITER)
+            .disable(StreamReadFeature.USE_FAST_DOUBLE_PARSER)
+            .disable(StreamWriteFeature.USE_FAST_DOUBLE_WRITER)
             .build();
-        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
-        assertTrue(f.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
+        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertFalse(f.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
+
         final CsvMapper mapper = mapperForCsv(f);
         JsonParser parser = mapper.createParser(new StringReader(""));
-        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
-        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
         JsonGenerator generator = mapper.createGenerator(new StringWriter());
-        assertTrue(generator.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
+        assertFalse(generator.isEnabled(StreamWriteFeature.USE_FAST_DOUBLE_WRITER));
     }
 
     public void testFactoryBuilderFastBigNumberFeature() throws Exception
     {
         CsvFactory f = CsvFactory.builder()
-                .enable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
+                .disable(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER)
                 .build();
-        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
-        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertFalse(f.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(f.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
         final CsvMapper mapper = mapperForCsv(f);
         JsonParser parser = mapper.createParser(new StringReader(""));
-        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
-        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+        assertFalse(parser.isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+        assertTrue(parser.isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
     }
 }
