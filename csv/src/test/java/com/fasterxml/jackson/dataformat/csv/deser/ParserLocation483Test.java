d076aab066825a6b7d6fd04f36d55861803a7bd1
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-08-21 17:31:43 -0700

    Fix #483: improve location tracking for CSV (#493)

---
+++ csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/ParserLocation483Test.java
@@ -0,0 +1,30 @@
+package com.fasterxml.jackson.dataformat.csv.deser;
+
+import java.util.*;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.ModuleTestBase;
+
+public class ParserLocation483Test extends ModuleTestBase
+{
+    private final CsvMapper MAPPER = mapperForCsv();
+
+    // [dataformats-text#483]: Location incorrect
+    public void testAsSequence() throws Exception
+    {
+        try (MappingIterator<List<String>> reader = MAPPER
+                .readerForListOf(String.class)
+                .with(CsvParser.Feature.WRAP_AS_ARRAY)
+                .readValues("name,dob\n\"string without end")) {
+            reader.readAll();
+        } catch (JacksonException e) {
+            verifyException(e, "Missing closing quote");
+            assertEquals(2, e.getLocation().getLineNr());
+            // This is not always accurate but should be close:
+            assertEquals(20, e.getLocation().getColumnNr());
+        }
+    }
+}
