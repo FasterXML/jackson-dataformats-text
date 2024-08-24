9623b2f8e0086c3ca727b9a8fbe178a48d8d2f09
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-08-24 16:16:00 -0700

    Some incremental work

--- csv/src/test/java/com/fasterxml/jackson/dataformat/csv/failing/ReadBracketedArray442Test.java
+++ csv/src/test/java/com/fasterxml/jackson/dataformat/csv/failing/ReadBracketedArray442Test.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.databind.MappingIterator;
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType;
+import com.fasterxml.jackson.dataformat.csv.CsvValueDecorator;
 import com.fasterxml.jackson.dataformat.csv.ModuleTestBase;
 
 // [dataformats-text#442]
@@ -38,9 +40,42 @@ public class ReadBracketedArray442Test extends ModuleTestBase
     public void testArrayWithBracketsRead() throws Exception
     {
         byte[] input = readResource("/data/story-100.csv");
-        CsvSchema schema = MAPPER.schemaFor(Article.class)
+
+        // Test with schema constructed a few different ways
+        _testArrayWithBracketsRead(input, _automaticSchema());
+        _testArrayWithBracketsRead(input, _manualSchema(ColumnType.STRING));
+        _testArrayWithBracketsRead(input, _manualSchema(ColumnType.ARRAY));
+    }
+
+    private CsvSchema _automaticSchema()
+    {
+        return MAPPER.schemaFor(Article.class)
                 .withHeader()
                 .withArrayElementSeparator(",");
+    }
+
+    private CsvSchema _manualSchema(ColumnType ct)
+    {
+        // second schema: manual construction
+        return CsvSchema.builder()
+                .addColumn("id", ColumnType.STRING)
+                .addColumn("title", ColumnType.STRING)
+                .addColumn("author", ColumnType.STRING)
+                .addColumn("comments", ColumnType.NUMBER)
+                .addColumn("score", ColumnType.NUMBER)
+                .addColumn("time", ColumnType.NUMBER)
+                // and then the interesting one; may mark as "String" or "Array"
+                .addColumn("embedding", ct,
+                        col -> col.withValueDecorator(_bracketDecorator()))
+                .build();
+    }
+
+    private CsvValueDecorator _bracketDecorator() {
+        return null;
+    }
+
+    private void _testArrayWithBracketsRead(byte[] input, CsvSchema schema) throws Exception
+    {
         MappingIterator<Article> it = MAPPER.readerFor(Article.class)
                 .with(schema)
                 .readValues(input);
