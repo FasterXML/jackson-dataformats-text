a9e9bc142235d836cb51d02e3a4a4538c64898ca
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-01-11 22:02:53 +0900

    Remove JUnit 4 assertion

--- properties/src/test/java/com/fasterxml/jackson/dataformat/javaprop/BinaryParsingTest.java
+++ properties/src/test/java/com/fasterxml/jackson/dataformat/javaprop/BinaryParsingTest.java
@@ -6,8 +6,7 @@ import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinaryParsingTest extends ModuleTestBase
 {
