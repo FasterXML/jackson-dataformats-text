e306fa64a1ece04de10c2b3eb4ed25ab4a6a81a3
PJ Fanning <pjfanning@users.noreply.github.com>
2025-02-26 03:39:44 +0100

    add stream test to csv test case 497 (#541)

--- csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/FuzzCSVReadTest.java
+++ csv/src/test/java/com/fasterxml/jackson/dataformat/csv/deser/FuzzCSVReadTest.java
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.dataformat.csv.deser;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -14,18 +15,29 @@ import static org.assertj.core.api.Assertions.fail;
 public class FuzzCSVReadTest extends StreamingCSVReadTest
 {
     private final CsvMapper CSV_MAPPER = mapperForCsv();
+    private final byte[] INPUT = new byte[] { 0x20, (byte) 0xCD };
 
     // https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=50036
     @Test
     public void testUTF8Decoding50036() throws Exception
     {
-        byte[] INPUT = new byte[] { 0x20, (byte) 0xCD };
         try {
             CSV_MAPPER.readTree(INPUT);
             fail("Should not pass");
         } catch (IOException e) {
             verifyException(e, "End-of-input after first 1 byte");
             verifyException(e, "of a UTF-8 character");
+        }
+    }
+
+    @Test
+    public void testUTF8Decoding50036Stream() throws Exception
+    {
+        try {
+            CSV_MAPPER.readTree(new ByteArrayInputStream(INPUT));
+            fail("Should not pass");
+        } catch (IOException e) {
+            verifyException(e, "Unexpected EOF in the middle of a multi-byte UTF-8 character");
         }
     }
 }
