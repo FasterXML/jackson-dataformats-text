4205f2146b3eeb57287e18cf7f34e87682e3bb40
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-03-06 14:20:56 -0800

    ...

--- csv/src/test/java/tools/jackson/dataformat/csv/tofix/UnicodeCSVRead497Test.java
+++ csv/src/test/java/tools/jackson/dataformat/csv/tofix/UnicodeCSVRead497Test.java
@@ -40,7 +40,7 @@ public class UnicodeCSVRead497Test extends ModuleTestBase
         assertNotNull(o);
         assertTrue(o.isArray());
         assertEquals(1, o.size());
-        assertEquals(o.get(0).textValue(), doc);
+        assertEquals(o.get(0).stringValue(), doc);
     }
 
     @Test
@@ -64,7 +64,7 @@ public class UnicodeCSVRead497Test extends ModuleTestBase
         assertNotNull(o);
         assertTrue(o.isArray());
         assertEquals(1, o.size());
-        assertEquals(o.get(0).textValue(), doc);
+        assertEquals(o.get(0).stringValue(), doc);
     }
 
     private static String buildTestString() {
