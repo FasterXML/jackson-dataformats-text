07aff9e72a479e0b4a015ff0b235f75689f59d37
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-11-20 09:53:51 -0800

    Add failing test for #568

---
+++ yaml/src/test/java/tools/jackson/dataformat/yaml/tofix/GeneratorWithMinimize568Test.java
@@ -0,0 +1,36 @@
+package tools.jackson.dataformat.yaml.tofix;
+
+import java.io.StringWriter;
+
+import org.junit.jupiter.api.Test;
+
+import tools.jackson.core.*;
+
+import tools.jackson.dataformat.yaml.ModuleTestBase;
+import tools.jackson.dataformat.yaml.YAMLFactory;
+import tools.jackson.dataformat.yaml.YAMLMapper;
+import tools.jackson.dataformat.yaml.YAMLWriteFeature;
+import tools.jackson.dataformat.yaml.testutil.failure.JacksonTestFailureExpected;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GeneratorWithMinimize568Test extends ModuleTestBase
+{
+    private final YAMLMapper MINIM_MAPPER = new YAMLMapper(YAMLFactory.builder()
+                .enable(YAMLWriteFeature.MINIMIZE_QUOTES)
+                .build());
+
+    // [dataformats-text#568]: snakeyaml-engine bug fixed in 3.x
+    @JacksonTestFailureExpected
+    @Test
+    void testLinefeedAsDoc() {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator generator = MINIM_MAPPER.createGenerator(writer)) {
+            generator.writeString("\n");
+        }
+        try (JsonParser parser = MINIM_MAPPER.createParser(writer.toString())) {
+            assertToken(JsonToken.VALUE_STRING, parser.nextToken());  // fails!
+            assertEquals("\n", parser.getString());
+        }
+    }
+}
